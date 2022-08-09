ff11b97f70de4917685ca279e133e677f799874a
Devesh <devesh181002@gmail.com>
2022-08-09 13:01:18 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int num1,int num2){
+    cout<<"Enter 1st number"<<endl;
+    cin>>num1;
+    cout<<"Enter 2nd number"<<endl;
+    cin>>num2;
+    int multiply=num1*num2;
+    cout<<"The multiplication of 2 numbers is "<<multiply<<endl;
+
+    
+
+
+
+    return 0;
+}
